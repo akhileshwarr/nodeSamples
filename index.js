b5fdbcfca78c028af0bb53da74a13298965dfc02
akhileshwarr <akhileshwar.rachakonda1@wipro.com>
2018-09-26 20:15:12 +0530

    done eslinting

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 /* eslint-disable no-console */
-const http = require('http');
 // const fs = require('fs');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
 
-var multer = require('multer');
+const app = express();
+const bodyParser = require('body-parser');
+
+const multer = require('multer');
+
 const upload = multer({
-  dest: 'multer/' // this saves your file into a directory called "uploads"
+  dest: 'multer/', // this saves your file into a directory called "uploads"
 });
 const hostname = '127.0.0.1';
 const port = 3001;
-const routes = require('./routes')
+const routes = require('./routes');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
-app.get('/readfile',routes.readFromFile)
-app.get('/product/:a/:b',routes.product)
-app.post('/writefile',upload.single('test'),routes.writefile)
-app.get('/stringreg',routes.stringext)
+app.get('/readfile', routes.readFromFile);
+app.get('/product/:a/:b', routes.product);
+app.post('/writefile', upload.single('test'), routes.writefile);
+app.get('/stringreg', routes.stringext);
 
-if (process.env.NODE_ENV !== 'test')
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
 
 module.exports = app;
