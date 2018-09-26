cf4e902f23dd9ad5f7edcaa6ddbf1a900e9afdcc
akhileshwarr <akhileshwar.rachakonda1@wipro.com>
2018-09-26 14:57:56 +0530

    basics httpserver to read file contents

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 /* eslint-disable no-console */
 const http = require('http');
+// const fs = require('fs');
+const router = require('./router');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+router.register('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('hello world');
+});
+
+const server = http.createServer((req, res) => {
+  console.log(process.argv);
+  const handler = router.route(req);
+  handler.process(req, res);
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+module.exports = server;
